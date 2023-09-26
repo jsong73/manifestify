ff18322a0d6d45e42de1eff4dca92817f5460fe7
jsong73 <jessicajinsong@gmail.com>
2023-09-25 21:52:19 -0400

    apollo server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const app = express();
 const db = require("./config/connection")
-
+const{ ApolloServer } = require("@apollo/server")
+const { authMiddleware } = require("./utils/auth");
 const PORT = process.env.PORT || 3004;
+const { typeDefs, resolvers } = require("./schemas");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+
 
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-db.once("connected", () => {
+async function startApolloServer() {
+    const server = new ApolloServer({ typeDefs, resolvers, context: authMiddleware });
+    const { url } = await startStandaloneServer(server);
+    console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+  }
+
+  
+db.once("open", () => {
     app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}`)
-})
-})
+        console.log(`API server running on port ${PORT}!`);
+        });
+    });
+
+startApolloServer();

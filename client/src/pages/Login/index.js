6ebca52ecccec4fb688390e6a0afb7ca812c39dc
jsong73 <jessicajinsong@gmail.com>
2023-10-06 13:19:15 -0400

    sign up page

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,10 +1,92 @@
-import React from "react";
-
+import React, {useState} from "react";
+import Auth from "../../utils/Auth"
+import { LOGIN_USER } from "../../utils/mutations";
+import { useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+    const [formState, setFormState] = useState({
+        email: "",
+        password: "",
+      });
+    
+      const [ login, {error} ] = useMutation(LOGIN_USER);
+      
+      const handleChange = (event) => {
+        const {name, value} = event.target;
+    
+        setFormState({
+          ...formState,
+          [name]: value,
+        });
+      };
+    
+      const loginFormHandler = async (event) => {
+        event.preventDefault();
+        try{
+          const { data } = await login({
+            variables: {...formState},
+          });
+          Auth.login(data.login.token);
+        } catch (error) {
+          console.log(error)
+        }
+    
+        setFormState({
+          email: "",
+          password: "",
+          birthday:""
+        })
+      };
+    
     return (
-      <div>
-        LOGIN
+      <div className="circular-gradient text-white">
+        <div className="flex flex-col items-center justify-center h-screen text-center">
+            <h1 className="text-center text-3xl text-shadow"> Sign in </h1>
+
+            <p className="button mt-2 text-sm">  Or{' '}
+                <button> <Link to="/signup" className ="mt-2"> create an account </Link></button>
+            </p>
+
+
+            <form  
+             className="button mt-8 space-y-6" 
+             onSubmit= {loginFormHandler}>
+
+            <input
+                className="w-5/6 bg-transparent border border-white rounded-full px-4 py-4"
+                placeholder="Email"
+                name="email"
+                type="email"
+                autoComplete="off"
+                value= {formState.email}
+                onChange={handleChange}/>
+
+            <input
+                className="w-5/6 bg-transparent border border-white rounded-full px-4 py-4"
+                placeholder="Password"
+                name="password"
+                type="password"
+                autoComplete="off"
+                value= {formState.password}
+                onChange={handleChange}/>
+
+            
+            {error && (
+                <div className="mt-2 text-center text-sm text-red-600">
+                    {error.message.includes("duplicate key value") ? (
+                        "Email already exists."
+                        ) : (
+                        "Please fill out all required fields."
+                    )}
+                </div>
+            )}
+
+              <button 
+              className="text-md text-shadow border border-white rounded-full px-4 py-4 mt-12" type="submit"> Welcome back </button>
+            </form>
+
+        </div>
       </div>
     );
   };
--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import Auth from "../../utils/Auth"
 import { ADD_USER } from "../../utils/mutations";
 import { useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
     const [formState, setFormState] = useState({
@@ -10,7 +11,7 @@ const Signup = () => {
         birthday: "",
       });
     
-      const [ addUser] = useMutation(ADD_USER);
+      const [ addUser, {error} ] = useMutation(ADD_USER);
       
       const handleChange = (event) => {
         const {name, value} = event.target;
@@ -42,7 +43,12 @@ const Signup = () => {
     return (
       <div className="circular-gradient text-white">
         <div className="flex flex-col items-center justify-center h-screen text-center">
-                <h1 className="text-center text-3xl text-shadow">Create a new account</h1>
+            <h1 className="text-center text-3xl text-shadow">Create a new account</h1>
+
+            <p className="button mt-2 text-sm">  Or{' '}
+                <button> <Link to="/login" className ="mt-2"> continue to sign in </Link></button>
+            </p>
+
 
             <form  
              className="button mt-8 space-y-6" 
@@ -74,11 +80,21 @@ const Signup = () => {
                 autoComplete="off"
                 value= {formState.birthday}
                 onChange={handleChange}/>
-
+            
+            {error && (
+                <div className="mt-2 text-center text-sm text-red-600">
+                    {error.message.includes("duplicate key value") ? (
+                        "Email already exists."
+                        ) : (
+                        "Please fill out all required fields."
+                    )}
+                </div>
+            )}
 
               <button 
               className="text-md text-shadow border border-white rounded-full px-4 py-4 mt-12" type="submit"> Create account </button>
             </form>
+
         </div>
       </div>
     );
